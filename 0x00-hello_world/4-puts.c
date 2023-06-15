4b81001dc41a30e966da2b2472a20c1712ada149
root <okoreq254@gmail.com>
2023-06-15 13:24:21 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+/* Print the desired output using the puts function */
+puts("\"Programming is like building a multilingual puzzle");
+
+/* Return 0 to show successful execution */
+return 0;
+}
