4f652cf0b5b4311f1e7011febc040ade51c2ea47
root <okoreq254@gmail.com>
2023-06-15 15:39:05 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ *  main - Entry point
  *
- * Return: Always 0 (success)
+ *  Return: Always 0 (success)
 */
 int main(void)
+
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 
