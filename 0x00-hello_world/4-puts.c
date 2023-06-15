0738456fc72cc7471a9efb45aa3609399a66c0ff
root <okoreq254@gmail.com>
2023-06-15 13:49:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-int main(void) {
-// Print the desired string using the puts function
+int main(void) 
+{
 
-    put("\"Programming is like building a multilingual puzzle");
+/* Print the desired string using the puts function */
+    puts("Programming is like building a multilingual puzzle");
 
-    return 0;
+/* Return 0 to indicate successful execution */
+    return (0);
 }
