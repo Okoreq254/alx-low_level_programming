dc779f29926fd1e19c34617c60dee2183a363c9e
root <okoreq254@gmail.com>
2023-06-16 18:29:04 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,21 +6,21 @@
 */
 int main(void)
 {
-	char y;
+	int x, y;
 
-	char x;
+	for (x = 0; x < 9; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
 
-	y = 'a';
-	x = 'A';
-	while
-		(y <= 'z')
-	{
-		putchar(y) y++;
-	}
-	while
-		(x <= 'z')
-	{
-		putchar(x) x++;
+			if (x != 8 || y != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
