52679979e630c3d362ade7dea13fc83aa47a7646
root <okoreq254@gmail.com>
2023-06-16 18:16:31 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char y;
+
+	char x;
+
+	y = 'a';
+	x = 'A';
+	while
+		(y <= 'z')
+	{
+		putchar(y); y++
+	}
+	while
+		(x <= 'z')
+	{
+		putchar(x); x++
+	}
+	putchar('\n');
+	return (0);
+}
+
