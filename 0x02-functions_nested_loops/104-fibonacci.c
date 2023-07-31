897e6a8ae12b6bd226df992cb56c49b0abaa6873
Okoreq254 <okoreq254@gmail.com>
2023-07-31 12:31:55 +0300

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long int prev = 1, curr = 2, next;
+	unsigned long int limit = 96;
+	int i;
+
+	printf("%lu, %lu, ", prev, curr);
+
+	for (i = 3; i <= limit; i++)
+	{
+		next = prev + curr;
+		prev = curr;
+		curr = next;
+
+		if (curr >= 100)
+			printf("%lu", curr);
+		else
+			printf("0%lu", curr);
+
+		if (i != limit)
+			printf(", ");
+	}
+
+	putchar('\n');
+	return (0);
+}
